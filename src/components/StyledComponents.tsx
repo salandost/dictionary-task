dedefc591728ec5717ab267fa5a06945f1e66060
Aram Babayan <arambabayan95@gmail.com>
2021-05-19 11:21:46 +0400

    styling fixes

--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -2,12 +2,16 @@ import styled from "@emotion/styled";
 import color from "../styles/color";
 
 export const MainContainer = styled.div`
-  padding-top: 30px;
+  padding: 30px 16px 0;
   display: flex;
   align-items: center;
   flex-flow: column;
   margin: 0 auto;
-  width: 762px;
+  width: 100%;
+  max-width: 768px;
+  @media (max-width: 768px) {
+    max-width: 480px;
+  }
 `;
 
 export const Header = styled.header`
@@ -16,4 +20,5 @@ export const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
 `;
